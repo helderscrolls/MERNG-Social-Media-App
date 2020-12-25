56e1dda1ce954e77faddba6e607f1d39036aa182
Helder Nascimento <helder.nascimento@ext.vallourec.com>
2020-12-25 18:01:26 +0100

    feat(posts): implement new post creation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 
+import { AuthContext } from '../context/auth';
 import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function Home() {
-  const { loading, data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY);
+  const { user } = useContext(AuthContext);
+  const { 
+    loading,
+    data: { getPosts: posts } = {}
+  } = useQuery(FETCH_POSTS_QUERY);
 
   return (
     <Grid columns={3}>
@@ -14,6 +20,11 @@ function Home() {
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading posts..</h1>
         ) : (
@@ -27,26 +38,5 @@ function Home() {
     </Grid>
   )
 }
-
-const FETCH_POSTS_QUERY = gql`
-  {
-      getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }`;
 
 export default Home;
